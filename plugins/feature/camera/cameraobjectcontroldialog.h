c8541c0d00aca70aa0343802d0aa95a4dab1b431
Jon Beniston <jon@beniston.com>
2026-04-27 14:13:31 +0100

    Update copyright

--- plugins/feature/camera/cameraobjectcontroldialog.h
+++ plugins/feature/camera/cameraobjectcontroldialog.h
@@ -1,5 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////////
 // Copyright (C) 2026 Jon Beniston, M7RCE <jon@beniston.com>                     //
+// Some code by AI                                                               //
 //                                                                               //
 // This program is free software; you can redistribute it and/or modify          //
 // it under the terms of the GNU General Public License as published by          //

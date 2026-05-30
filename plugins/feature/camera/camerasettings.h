a26f9253aeed89cebe06bfb04d628b93e731d2df
Jon Beniston <jon@beniston.com>
2026-05-30 20:15:10 +0100

    Increase camera plate solve magnitude limit

--- plugins/feature/camera/camerasettings.h
+++ plugins/feature/camera/camerasettings.h
@@ -294,7 +294,7 @@ struct CameraSettings
     static constexpr double m_minStarAspectRatio = 1.0;
     static constexpr double m_maxStarAspectRatio = 10.0;
     static constexpr double m_minPlateSolveMagnitude = -2.0;
-    static constexpr double m_maxPlateSolveMagnitude = 18.0;
+    static constexpr double m_maxPlateSolveMagnitude = 20.0;
     static constexpr int m_minPlateSolveMatches = 2;
     static constexpr int m_maxPlateSolveMatches = 32;
     static constexpr double m_minPlateSolveMatchRadius = 1.0;

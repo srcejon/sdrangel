f4b876a2f6d9053a6de81050a67b3abc169a2d7c
Jon Beniston <jon@beniston.com>
2026-06-12 13:40:56 +0100

    Tune video playback audio cushion

--- plugins/feature/camera/cameraqtaudiocontroller.h
+++ plugins/feature/camera/cameraqtaudiocontroller.h
@@ -58,7 +58,7 @@ public:
     void clearMonitorAudio();
     void prefillMonitorAudio(int milliseconds);
     static int filePlaybackMonitorPrefillMs() { return 60; }
-    static int filePlaybackMonitorTargetFillMs() { return 25; }
+    static int filePlaybackMonitorTargetFillMs() { return 30; }
     void resetMonitorDebugStats();
     void submitPcmSamples(const QByteArray& pcmS16Stereo, int sampleRate);
     void submitMonitorPcmSamples(const QByteArray& pcmS16Stereo, int sampleRate);
